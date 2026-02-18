dabaea5dd25a011abac377df95a2c19b7694b84d
asherstubbs <asher_stubbs@yahoo.com>
2026-02-18 00:42:54 -0500

    Add files via upload

---
+++ AStubbs-CPT-206-A80H-Lab3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AStubbs_CPT_206_A80H_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'aStubbsStateDatabaseDataSet.States' table. You can move, or remove it, as needed.
+            this.statesTableAdapter.Fill(this.aStubbsStateDatabaseDataSet.States);
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            DataRowView row = cbStatePicked.SelectedItem as DataRowView;
+            if (row != null)
+            {
+                Report window = new Report();
+                window.lblState.Text = row["States"].ToString();
+                window.lblCapital.Text = row["Capital"].ToString();
+                window.lblThree.Text = row["Largest Three Cities"].ToString();
+                window.lblBird.Text = row["Bird"].ToString();
+                window.lblFlower.Text = row["Flower"].ToString();
+                window.lblPop.Text = row["Population"].ToString();
+                window.lblIncome.Text = "$"+row["Median Income"].ToString();
+                window.lblDesc.Text = row["Flag Description"].ToString();
+                window.lblPercent.Text = row["% of Tech Jobs"].ToString();
+                window.Show();
+            }
+        }
+    }
+}
---
+++ AStubbs-CPT-206-A80H-Lab3/StateLibrary/State.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateLibrary
+{
+    public class State
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public string FlagDesc { get; set; }
+        public string Flower { get; set; }
+        public string Bird { get; set; }
+        public string Three { get; set; }
+        public string Capital { get; set; }
+        public double Income { get; set; }
+        public double Percent { get; set; }
+
+        public State(int iD, string name, int population, string flagDesc, string flower, string bird, string three, string capital, double income, double percent)
+        {
+            ID = iD;
+            Name = name;
+            Population = population;
+            FlagDesc = flagDesc;
+            Flower = flower;
+            Bird = bird;
+            Three = three;
+            Capital = capital;
+            Income = income;
+            Percent = percent;
+
+
+        }
+    }
+}
+
